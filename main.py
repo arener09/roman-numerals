a92ab25016cb0c2b844a41d2d83f3f6e70b23c19
arener09 <a.reinhardbrenner@gmail.com>
2026-05-02 09:52:30 +0200

    refactor: Basis-Mapping als Modulkonstante _BASIS

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+_BASIS = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+
+
 def transform_to_roman(n: int) -> str:
     """Konvertiert eine arabische Zahl in römische Notation (1–3999)."""
-    basis = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
-    return basis.get(n, "")
+    return _BASIS.get(n, "")
